997c29f0832ba0175e36a7e592656ffbd110c4ce
rahcode <rahcode.ir@gmail.com>
2020-05-06 17:39:31 +0430

    add splash activity and check internet network method

--- app/src/main/java/ir/rahcode/persisapp/Splash/SplashActivty.java
+++ app/src/main/java/ir/rahcode/persisapp/Splash/SplashActivty.java
@@ -1,16 +1,68 @@
 package ir.rahcode.persisapp.Splash;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import ir.rahcode.persisapp.R;
+import ir.rahcode.persisapp.utils.ConnectivityUtils;
 
 public class SplashActivty extends AppCompatActivity {
+
+    private Boolean ConnectionAvibale = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_activty);
+
+        ChekInternetConection();
+            
+    }
+
+    private void ChekInternetConection() {
+        ConnectionAvibale = ConnectivityUtils.isConnected(this);
+        if (!ConnectionAvibale) {
+            showPopupHold(getString(R.string.NO_INTERNET));
+        }
+    }
+
+    private void OpenInternetPage(String type) {
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        if (type == "GPRS")
+            intent.setClassName("com.android.phone", "com.android.phone.NetworkSetting");
+        else
+            intent.setClassName("com.android.settings", "com.android.settings.wifi.WifiSettings");
+        startActivity(intent);
+    }
+
+
+    private void showPopupHold(String message) {
+        final AlertDialog popup = new AlertDialog.Builder(this).create();
+        popup.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        View viewPopup = getLayoutInflater().inflate(R.layout.popup, null);
+        TextView title = viewPopup.findViewById(R.id.textView48);
+        title.setText(message);
+        TextView ok = viewPopup.findViewById(R.id.GPRS);
+        ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                OpenInternetPage("GPRS");
+            }
+        });
+        popup.setView(viewPopup);
+        popup.show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        ChekInternetConection();
     }
 }
