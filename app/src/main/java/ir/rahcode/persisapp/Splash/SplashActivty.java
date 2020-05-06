87c82c66d40cb04bc22001fd7e5f07d153399c06
mohsen yousefi <rahcode.ir@gmail.com>
2020-05-06 16:05:38 +0430

    Initial commit

---
+++ app/src/main/java/ir/rahcode/persisapp/Splash/SplashActivty.java
@@ -0,0 +1,16 @@
+package ir.rahcode.persisapp.Splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ir.rahcode.persisapp.R;
+
+public class SplashActivty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_activty);
+    }
+}
